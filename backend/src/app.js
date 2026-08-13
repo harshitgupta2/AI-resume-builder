966178d4986f21d76423f37812aa1059eeec238d
Harshit Gupta <harshitgupta4426@gmail.com>
2026-08-13 13:38:02 +0530

    fix the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,10 @@ const app = express();
 
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:[
+        'http://localhost:5173',
+        'https://ai-resume-builder-indol-eta.vercel.app'
+    ],
     credentials:true
 }));
 app.use(express.json());
