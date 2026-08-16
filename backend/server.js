12495784d251b07a74d44f764ed6c17578b4bff1
Harshit Gupta <harshitgupta4426@gmail.com>
2026-08-16 12:54:43 +0530

    fix the path issue

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import app from "./src/app.js";
 import config from "./src/config/env.js";
-import connectDb from '../backend/src/config/db.js';
+import connectDb from './src/config/db.js';
 
 
 
 connectDb();
 
 
-
+console.log(config.PORT)
 app.listen(config.PORT,()=>{
     console.log(`server is listing on ${config.PORT}`)
 })
